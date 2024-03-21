d2165569da8b9e565a5939ff16d1c82bfd486e2d
Terminal <terminalkarabo@gmail.com>
2024-03-20 21:09:16 -0700

    fix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 
 def square_matrix_map(matrix=[]):
-    return list(map(lambda num1: list(map(lambda num2: num2**2, num1)), matrix))
+    return (
+        list(
+            map(
+                lambda num1: list(
+                    map(lambda num2: num2**2, num1)
+                ),
+                matrix
+            )
+        )
+    )
