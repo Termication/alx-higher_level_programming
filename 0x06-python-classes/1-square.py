34974eba2847b7f9564c5275d3af8db1ae51d480
Terminal <terminalkarabo@gmail.com>
2024-04-02 05:03:25 -0700

    class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class defined"""
+
+
+class Square:
+    """representing the square
+    """
+    def __init__(self, size):
+        """Initialization of a square class
+        Args:
+            size (int): size of a side of the square as defined
+        Returns: nothing
+        """
+        self.__size = size
