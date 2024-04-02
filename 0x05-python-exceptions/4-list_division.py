d491acf6ae78e8e163b5a5e279357b91d24fc825
Terminal <terminalkarabo@gmail.com>
2024-04-02 14:28:55 -0700

    function that divides element by element 2 lists.

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,39 +1,17 @@
 #!/usr/bin/python3
-"""
-List Division Module
-
-This module defines a function list_division that divides two lists
-element-wise and returns a new list containing the results.
-"""
-
-
 def list_division(my_list_1, my_list_2, list_length):
-    """
-    Divides two lists element-wise.
-
-    Args:
-        my_list_1 (list): The first list.
-        my_list_2 (list): The second list.
-        list_length (int): The number of elements to divide.
-
-    Returns:
-        list: A new list containing the results of the divisions.
-    """
-    result_list = []
-
-    for i in range(list_length):
+    divisions = []
+    for index in range(list_length):
+        result = 0
         try:
-            division = my_list_1[i] / my_list_2[i]
-        except TypeError:
-            print("TypeError: Cannot divide elements of different types.")
-            division = 0
+            result = my_list_1[index] / my_list_2[index]
         except ZeroDivisionError:
-            print("ZeroDivisionError: Cannot divide by zero.")
-            division = 0
+            print('division by 0')
+        except TypeError:
+            print('wrong type')
         except IndexError:
-            print("IndexError: Lists are of different lengths.")
-            division = 0
+            print('out of range')
         finally:
-            result_list.append(division)
+            divisions.append(result)
 
-    return result_list
+    return divisions
