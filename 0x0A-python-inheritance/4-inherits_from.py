efa8058fc3fb754dc821c03b0fa35bb960b437dd
Terminal <terminalkarabo@gmail.com>
2024-04-15 16:33:12 -0700

    pycodestyle fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,5 +6,6 @@ Defines the inherits_from function.
 
 def inherits_from(obj, a_class):
     """Returns True if obj is a subclass of
-    a_class, otherwise returns False."""
+    a_class, otherwise returns False.
+    """
     return issubclass(type(obj), a_class) and type(obj) != a_class
