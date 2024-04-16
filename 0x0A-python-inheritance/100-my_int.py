6ccf306a254842471c335192a1b8a20b471bfe8a
Terminal <terminalkarabo@gmail.com>
2024-04-15 17:07:04 -0700

    class MyInt that inherits from int:
    MyInt is a rebel. MyInt has == and != operators inverted

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Defines the MyInt class.
+"""
+
+
+class MyInt(int):
+    """Represents a rebellious version of an integer, perfect for opposite day!"""
+    def __new__(cls, *args, **kwargs):
+        """Creates a new instance of the class."""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
+    def __eq__(self, other):
+        """Defines the opposite behavior for equality."""
+        return int(self) != other
+
+    def __ne__(self, other):
+        """Defines the opposite behavior for inequality."""
+        return int(self) == other
