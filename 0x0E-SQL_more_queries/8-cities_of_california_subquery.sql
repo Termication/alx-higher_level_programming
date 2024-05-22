0e63792bc76980df90b2c32bf65781779948daf3
Terminal <terminalkarabo@gmail.com>
2024-05-22 12:26:42 -0700

    a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
    The states table contains only one record where name = California (but the id can be different, as per the example)
    Results must be sorted in ascending order by cities.id
    You are not allowed to use the JOIN keyword

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- A script that lists all cities of CA in the database hbtn_0d_usa and results are ordered by ascending cities.id.
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	        FROM `states`
+	       WHERE `name` = "California")
+ORDER BY `id`;
