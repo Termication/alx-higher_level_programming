109894aeb3803a7d56bd1369979ff6fff5e39ceb
Terminal <terminalkarabo@gmail.com>
2024-05-23 06:07:08 -0700

    a script that lists all Comedy shows in the database hbtn_0d_tvshows.

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,11 +1,11 @@
--- This query selects all comedy shows from the 'hbtn_0d_tvshows' database.
--- The results are sorted in descending order by the show title.
+-- This script lists all Comedy shows in the specified database.
+-- Results are sorted in ascending order by the show title.
 
-SELECT t.`title`
-  FROM `tv_shows` AS t
-       INNER JOIN `tv_show_genres` AS s
-       ON t.`id` = s.`show_id`
-       INNER JOIN `tv_genres` AS g
-       ON g.`id` = s.`genre_id`
- WHERE g.`name` = "Comedy"
- ORDER BY t.`title` DESC;
+SELECT t.title
+FROM tv_shows AS t
+INNER JOIN tv_show_genres AS s
+ON t.id = s.show_id
+INNER JOIN tv_genres AS g
+ON s.genre_id = g.id
+WHERE g.name = 'Comedy'
+ORDER BY t.title;
