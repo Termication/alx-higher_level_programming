c7f3a0cd051a2f33ba8ccc6188b5bbbcb8874d4f
Terminal <terminalkarabo@gmail.com>
2024-06-28 10:53:49 -0700

    a script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,45 +1,28 @@
 #!/usr/bin/python3
-import sys
+"""
+Script that accept an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+"""
+
 import MySQLdb
+from sys import argv
 
+if __name__ == '__main__':
+    """
+    get the states
+    from the database.
+    """
 
-def main():
-    # Parse the command line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
+    data = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    # Connect to the MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
-    )
+    cur = data.cursor()
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(argv[4]))
+    rows = cur.fetchall()
 
-    # Create a cursor object to execute queries
-    cursor = db.cursor()
-
-    # Create the query using format to include the user input
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        state_name)
-
-    # Execute the query
-    cursor.execute(query)
-
-    # Fetch all the rows
-    rows = cursor.fetchall()
-
-    # Display the results
     for row in rows:
         print(row)
-
-    # Close the cursor and connection
-    cursor.close()
-    db.close()
-
-
-if __name__ == "__main__":
-    main()
