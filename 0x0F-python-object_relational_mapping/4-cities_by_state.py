f979add836318a16ffddc040a13ef568dd713de3
Terminal <terminalkarabo@gmail.com>
2024-06-28 11:05:37 -0700

    pycodestyle inforced

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     """
 
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                           passwd=argv[2], db=argv[3])
+                         passwd=argv[2], db=argv[3])
 
     with db.cursor() as cur:
         cur.execute("""
