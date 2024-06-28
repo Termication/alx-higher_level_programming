24d9a5ca5d641500de5ed83c683f71f6de2b765c
Terminal <terminalkarabo@gmail.com>
2024-06-28 10:38:02 -0700

    a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
    Your script should take 4 arguments: mysql username, mysql password, database name and state name searched (no argument validation needed)
    You must use the module MySQLdb (import MySQLdb)
    Your script should connect to a MySQL server running on localhost at port 3306
    You must use format to create the SQL query with the user input
    Results must be sorted in ascending order by states.id
    Results must be displayed as they are in the example below
    Your code should not be executed when imported

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,45 @@
 #!/usr/bin/python3
-"""
-Script that accept an argument and
-displays all values in the states
-where `name` matches the argument
-from the database `hbtn_0e_0_usa`.
-"""
-
+import sys
 import MySQLdb
-from sys import argv
 
-if __name__ == '__main__':
-    """
-    get the states
-    from the database.
-    """
 
-    data = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                           passwd=argv[2], data=argv[3])
+def main():
+    # Parse the command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    cur = data.cursor()
-    cur.execute("SELECT * FROM states \
-                 WHERE name LIKE BINARY '{}' \
-                 ORDER BY states.id ASC".format(argv[4]))
-    rows = cur.fetchall()
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
 
+    # Create a cursor object to execute queries
+    cursor = db.cursor()
+
+    # Create the query using format to include the user input
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+        state_name)
+
+    # Execute the query
+    cursor.execute(query)
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Display the results
     for row in rows:
         print(row)
+
+    # Close the cursor and connection
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
