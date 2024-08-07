0d6aff0633d6071f72561a35f6ec968e0924ecc4
innocent <terminalkarbo@gmail.com>
2024-08-07 13:50:07 -0700

    a JavaScript script that updates the text of the <header> element to New Header!!! when the user clicks on DIV#update_header

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,4 @@
+// When the DIV with id "update_header" is clicked, change the text of the HEADER element to "New Header!!!".
+$('DIV#update_header').click(function () {
+  $('HEADER').text('New Header!!!');
+});
