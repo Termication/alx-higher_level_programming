528904d47a5d2bca3a8f68a8c866ce2c6ed68a4f
innocent <terminalkarbo@gmail.com>
2024-08-07 13:42:30 -0700

    a JavaScript script that toggles the class of the <header> element when the user clicks on the tag DIV#toggle_header:

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,4 @@
+// When the DIV with id "toggle_header" is clicked, toggle the classes 'green' and 'red' on the HEADER element.
+$('DIV#toggle_header').click(function () {
+  $('HEADER').toggleClass('green red');
+});
