3af15fa5730a649361c43f00cc64a0beeeefba6e
innocent <terminalkarabo@gmail.com>
2024-08-25 12:58:41 -0700

    a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
 # This script sends a request to a URL passed as an argument,
 # and displays only the status code of the response.
-
-# Send a request to the provided URL ($1)
-# -s: Silent mode (suppress progress meter and error messages)
-# -o /dev/null: Redirect the response body to /dev/null (ignore it)
-# --write-out "%{http_code}": Output the HTTP status code of the response
 curl -so /dev/null --write-out "%{http_code}" "$1"
