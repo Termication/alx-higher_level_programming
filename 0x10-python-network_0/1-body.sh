b0b46e7e263f9c8845e797422d3686651dc8aa30
innocent <terminalkarabo@gmail.com>
2024-08-25 11:55:41 -0700

    e a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
 # This script takes a URL as an argument, sends a GET request to the URL,
 # and displays the body of the response.
-
-# Send a GET request to the provided URL ($1)
-# -s: Silent mode (suppress progress meter and error messages)
-# -L: Follow redirects if the requested URL has moved
 curl -sL "$1"
