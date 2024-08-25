3a1312b3b7145acec8458b5177baf10704a9be8b
innocent <terminalkarabo@gmail.com>
2024-08-25 12:37:29 -0700

    a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
 # This script takes a URL as an argument, sends a POST request to the URL,
 # and displays the body of the response.
-# Two variables must be sent in the request body:
-# - email with the value 'test@gmail.com'
-# - subject with the value 'I will always be here for PLD'
-
-# Send a POST request to the provided URL ($1)
-# -s: Silent mode (suppress progress meter and error messages)
-# -X POST: Specify the request method as POST
-# -d "email=test@gmail.com&subject=I will always be here for PLD": Send the specified data in the request body
-curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
