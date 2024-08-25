6c96b715c0df58033c33e4d1a2128a72ee646ca9
innocent <terminalkarabo@gmail.com>
2024-08-25 12:10:53 -0700

    a Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-# This script sends a DELETE request to the URL passed as the first argument,
-# and displays the body of the response.
-
-# Send a DELETE request to the provided URL ($1)
-# -s: Silent mode (suppress progress meter and error messages)
-# -X DELETE: Specify the request method as DELETE
+# This script sends a DELETE request to the URL passed as the first argument
 curl -sX DELETE "$1"
