5fc97d1d021eb56e273a2fa530cf3a84ce4a3b6d
innocent <terminalkarabo@gmail.com>
2024-08-25 06:00:44 -0700

    Bash script that uses curl to send a request to a given URL and display the size of the response body in bytes.

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
 # Get the URL from the command-line argument
-URL=$1
-
 # Send a request to the URL, retrieve the response, and extract the size of the body in bytes
-curl -s -o /dev/null -w "%{size_download}\n" "$URL"
+curl -s "$1" | wc -c
