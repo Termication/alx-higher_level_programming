73fc681fbd03dc6e1f94d5004b6d05cdb1edb373
innocent <terminalkarabo@gmail.com>
2024-08-25 12:23:12 -0700

    a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -2,4 +2,4 @@
 # This script takes a URL as an argument, sends a GET request to the URL,
 # and displays the body of the response.
 # A header variable X-School-User-Id must be sent with the value
-curl -sX GET -H "X-School-User-Id: 98" "$1"
+curl -sH "X-School-User-Id: 98" "$1"
