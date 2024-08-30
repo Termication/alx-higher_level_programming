4e978b241f0f586e0e0b46b2cf38e6dbd247b89f
innocent <terminalkarabo@gmail.com>
2024-08-30 08:02:19 -0700

    0-hbtn_status.py pycodestyle fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,13 +6,16 @@ and displays the body response in a formatted manner.
 
 if __name__ == "__main__":
     import urllib.request as request
-    
+
     # Fetch the URL
     with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
-        
+
         # Print the body response details
         print('Body response:')
-        print("\t- type: {}".format(type(html)))        # Print the type of the response content
-        print("\t- content: {}".format(html))           # Print the raw content of the response
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))  # Print the decoded (UTF-8) content of the response
+        # Print the type of the response content
+        print("\t- type: {}".format(type(html)))
+        # Print the raw content of the response
+        print("\t- content: {}".format(html))
+        # Print the decoded (UTF-8) content of the response
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
